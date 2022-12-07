ae0f77792c200b72becbdf0d8251e27a1e67fd26
AlperenCivelekoglu <mehmetoglumehmet65@gmail.com>
2022-12-08 02:12:29 +0300

    commit

---
+++ pages/uyelik.vue
@@ -0,0 +1,17 @@
+<template>
+ <client-only placeholder="loading...">
+<div>
+    <TheHeader/>
+    <UyelikTab/>
+    <TheFooter/>
+</div>
+</client-only> 
+</template>
+
+<script>
+
+</script>
+
+
+
+
